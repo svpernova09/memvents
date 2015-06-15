70782d621dc32f2d3a7d7886e9c8823d2dbc1602
Joe Ferguson <joe@joeferguson.me>
2015-06-15 16:52:21 -0500

    Add events migration and model

---
+++ app/Event.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Memvents;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Event extends Model
+{
+
+    protected $table = 'events';
+
+    protected $fillable = [
+        'name',
+        'event_id',
+        'time',
+        'status',
+        'event_url',
+    ];
+}
---
+++ database/migrations/2015_06_15_214636_add_events_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('event_id');
+            $table->string('time');
+            $table->string('status');
+            $table->text('event_url');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('users');
+    }
+}
