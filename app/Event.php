a3458b44ee7405384299dcbe080e576eb4128ed5
Joe Ferguson <joe@joeferguson.me>
2015-06-16 18:46:59 -0500

    Add methods to get upcoming events based on date range

--- app/Event.php
+++ app/Event.php
@@ -2,8 +2,10 @@
 
 namespace Memvents;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Storage;
+
 class Event extends Model
 {
 
@@ -21,5 +23,35 @@ class Event extends Model
     public function getSampleEvents()
     {
         return Storage::get('sample_events.json');
+    }
+
+    public function getEventsWithin7Days()
+    {
+        $now = Carbon::now()->timestamp;
+        $future = Carbon::createFromTimestamp($now)->addWeeks(1)->timestamp;
+
+        return $this->whereBetween('time', [$now, $future])
+            ->where('status', 'upcoming')
+            ->get();
+    }
+
+    public function getEventsWithin3Days()
+    {
+        $now = Carbon::now()->timestamp;
+        $future = Carbon::createFromTimestamp($now)->addWeeks(3)->timestamp;
+
+        return $this->whereBetween('time', [$now, $future])
+            ->where('status', 'upcoming')
+            ->get();
+    }
+
+    public function getEventsWithin24Hours()
+    {
+        $now = Carbon::now()->timestamp;
+        $future = Carbon::createFromTimestamp($now)->addHours(24)->timestamp;
+
+        return $this->whereBetween('time', [$now, $future])
+            ->where('status', 'upcoming')
+            ->get();
     }
 }
