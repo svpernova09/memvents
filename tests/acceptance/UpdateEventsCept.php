a1ef9bbc5b2d2897f8187d720d3a082ec0b81f22
Joe Ferguson <joe@joeferguson.me>
2015-06-16 17:02:27 -0500

    Run our test in debug mode

--- tests/acceptance/UpdateEventsCept.php
+++ tests/acceptance/UpdateEventsCept.php
@@ -1,5 +1,5 @@
 <?php 
 $I = new AcceptanceTester($scenario);
 $I->wantTo('add or update events in database');
-$I->runShellCommand('php artisan memvents:update');
+$I->runShellCommand('php artisan memvents:update --debug');
 $I->seeInShellOutput('All Done!');
